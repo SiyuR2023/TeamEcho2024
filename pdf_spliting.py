b1d0dab07940bbd0b50fd8d8e62f6b6dcbad7fc7
Samuel Amao <s.amao.23@abdn.ac.uk>
2024-05-31 14:21:19 +0100

    added pdf_splitting code

---
+++ pdf_spliting.py
@@ -0,0 +1,34 @@
+import os
+from PyPDF2 import PdfReader, PdfWriter
+
+def split_pdf(file_path, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    
+    # Check if the PDF file exists
+    if not os.path.isfile(file_path):
+        print(f"Error: The file {file_path} does not exist.")
+        return
+    
+    # Open the PDF file
+    pdf_reader = PdfReader(file_path)
+    
+    for page_num in range(len(pdf_reader.pages)):
+        pdf_writer = PdfWriter()
+        pdf_writer.add_page(pdf_reader.pages[page_num])
+        
+        output_filename = os.path.join(output_dir, f'page_{page_num + 1}.pdf')
+        
+        with open(output_filename, 'wb') as out:
+            pdf_writer.write(out)
+        
+        print(f'Created: {output_filename}')
+
+if __name__ == "__main__":
+    # Path to the PDF file you want to split
+    pdf_file_path = 'pdf_uploadedFiles/centurion.pdf'  # Update 'example.pdf' to the actual file name
+    
+    # Directory where the single page PDFs will be saved
+    output_directory = 'pdf_outputFiles'
+    
+    split_pdf(pdf_file_path, output_directory)
